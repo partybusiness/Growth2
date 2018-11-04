2c2c602d8145e4b604e015597704cec2938b81ff
partybusiness <rrh@horriblepain.com>
2018-11-04 11:31:24 -0600

    Midi variation can be tested with keyboard input

--- Assets/Scripts/PlantSpawner.cs
+++ Assets/Scripts/PlantSpawner.cs
@@ -35,6 +35,15 @@ public class PlantSpawner : MonoBehaviour {
 	[SerializeField]
 	ColourRange[] colours;
 
+	[System.Serializable]
+	public class SimKey {
+		public KeyCode key;
+		public int keyIndex;
+	}
+
+	[SerializeField]
+	SimKey[] simKeys;
+
 	// Use this for initialization
 	void Start () {
 		lastPlants = new Dictionary<int, PlantGrower> ();
@@ -42,51 +51,57 @@ public class PlantSpawner : MonoBehaviour {
 			KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.F
 		};
 	}
-	
-	// Update is called once per frame
+
+	void SpawnFlower(int keyIndex) {
+		int octave = Mathf.FloorToInt(keyIndex / 12);
+		int keyInOctave = keyIndex % 12;
+
+		var newPlant = Instantiate (plantPrefab);
+		newPlant.plantSeed = Random.Range (0f, 20f);
+		newPlant.seedSpeed = 0.3f;
+		newPlant.flowerScale = Random.Range (0.5f, 1.5f); //adjust this according to attack?
+		newPlant.SetFlowerColour(Color.Lerp(colours[keyInOctave].colorOne,colours[keyInOctave].colorTwo,Random.Range(0f,1f)));
+		//newPlant.leafSpacing = Random.Range (-8, -3);
+		newPlant.leafCounter = Random.Range (-10, -3);
+		newPlant.transform.position = Vector3.Lerp(minPos,maxPos, (keyIndex*1f/numberOfKeys)+Random.Range(-0.1f,0.1f));//adjust this position?
+		lastPlants.Add(keyIndex, newPlant);
+	}
+
+	void ReleaseFlower(int keyIndex) {
+		if (lastPlants.ContainsKey(keyIndex) && lastPlants[keyIndex]!=null) {
+			lastPlants[keyIndex].seedSpeed = 0.9f;
+			lastPlants.Remove(keyIndex);
+		}
+	}
+
+	void CheckKey(int keyIndex) {
+		if (MidiMaster.GetKeyDown(keyIndex)) {
+			SpawnFlower (keyIndex);
+		}
+		if (MidiMaster.GetKeyUp (keyIndex)) {
+			ReleaseFlower (keyIndex);
+		}
+	}
+
+	void CheckSimKey(SimKey simkey) {
+		if (Input.GetKeyDown(simkey.key)) {
+			SpawnFlower (simkey.keyIndex);
+		}
+		if (Input.GetKeyUp (simkey.key)) {
+			ReleaseFlower (simkey.keyIndex);
+		}
+	}
+
+
 	void Update () {
 
 		for (int keyIndex = 0; keyIndex < numberOfKeys; keyIndex++) {
-			if (MidiMaster.GetKeyDown(keyIndex)) {
-				int octave = Mathf.FloorToInt(keyIndex / 12);
-				int keyInOctave = keyIndex % 12;
-
-				var newPlant = Instantiate (plantPrefab);
-				newPlant.plantSeed = Random.Range (0f, 20f);
-				newPlant.seedSpeed = 0.3f;
-				newPlant.flowerScale = Random.Range (0.5f, 1.5f); //adjust this according to attack?
-				newPlant.SetFlowerColour(Color.Lerp(colours[keyInOctave].colorOne,colours[keyInOctave].colorTwo,Random.Range(0f,1f)));
-				//newPlant.leafSpacing = Random.Range (-8, -3);
-				newPlant.leafCounter = Random.Range (-10, -3);
-				newPlant.transform.position = Vector3.Lerp(minPos,maxPos, (keyIndex*1f/numberOfKeys)+Random.Range(-0.1f,0.1f));//adjust this position?
-				lastPlants.Add(keyIndex, newPlant);
-			}
-			if (MidiMaster.GetKeyUp (keyIndex)) {
-				if (lastPlants.ContainsKey(keyIndex) && lastPlants[keyIndex]!=null) {
-					lastPlants[keyIndex].seedSpeed = 0.9f;
-					lastPlants.Remove(keyIndex);
-				}
-			}
+			CheckKey (keyIndex);
 		}
-/*		foreach (var key in allKeys) {
-			if (Input.GetKeyDown(key)) {
-				var newPlant = Instantiate (plantPrefab);
-				newPlant.plantSeed = Random.Range (0f, 20f);
-				newPlant.seedSpeed = 0.3f;
-				newPlant.flowerScale = Random.Range (0.5f, 1.5f); //adjust this according to attack
-				newPlant.SetFlowerColour(Random.ColorHSV(0f,0.2f,0.6f,1f,0.7f,1f));
-				//newPlant.leafSpacing = Random.Range (-8, -3);
-				newPlant.leafCounter = Random.Range (-10, -3);
-				newPlant.transform.position = Vector3.Lerp(minPos,maxPos, Random.Range(0f,1f));
-				lastPlants.Add(key, newPlant);
-			}
-			if (Input.GetKeyUp (key)) {
-				if (lastPlants.ContainsKey(key) && lastPlants[key]!=null) {
-					lastPlants[key].seedSpeed = 0.9f;
-					lastPlants.Remove(key);
-				}
-			}
-		}*/
+
+		foreach (var simKey in simKeys) {
+			CheckSimKey (simKey);
+		}
 
 	}
 }
