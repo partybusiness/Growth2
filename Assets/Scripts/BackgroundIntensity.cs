b83a7caa70109a8d11a80b6b38f1c3d816a0c0b8
partybusiness <rrh@horriblepain.com>
2018-11-04 16:44:46 -0600

    Imported background and willowisp

---
+++ Assets/Scripts/BackgroundIntensity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+
+public class BackgroundIntensity : MonoBehaviour {
+
+    SpriteRenderer spriteRenderer;
+    Color spriteColour;
+
+	// Use this for initialization
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteColour = spriteRenderer.color;
+        spriteColour.a = 0f;
+        spriteRenderer.color = spriteColour;
+        MidiJack.MidiDriver.Instance.noteOnDelegate += MidiKeyPress;
+	}
+
+    void MidiKeyPress (MidiChannel channel, int note, float velocity)
+    {
+        Debug.Log("Velocity: " + velocity);
+        if (spriteColour.a < velocity) {
+            spriteColour.a = velocity;
+        }
+    }
+
+    void Update()
+    {
+        spriteColour.a -= 0.01f;
+        spriteRenderer.color = spriteColour;
+    }
+}
---
+++ Assets/Scripts/WilloWispMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WilloWispMovement : MonoBehaviour {
+
+
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		var tempMove = new Vector3 (
+			(Mathf.PerlinNoise (0f, Time.time*0.1f) - 0.5f)*3f,
+			(Mathf.PerlinNoise (5.3f, Time.time*0.1f) - 0.5f)*0.7f,
+			               0);
+		if (tempMove.magnitude>0.35f)
+			transform.Translate (tempMove * Time.deltaTime*5f);
+	}
+}
