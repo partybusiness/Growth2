d5a96070c9ccc635d43359aa055c871f83d9b804
partybusiness <rrh@horriblepain.com>
2018-11-04 17:02:11 -0600

    Shows logo

---
+++ Assets/Scripts/Logo.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+
+public class Logo : MonoBehaviour {
+
+    public float timeUntilFadeIn = 10F;
+    public float fadeSpeed = 0.01F;
+
+    SpriteRenderer spriteRenderer;
+    Color colour;
+    float timer;
+
+	// Use this for initialization
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        colour = spriteRenderer.color;
+        MidiJack.MidiDriver.Instance.noteOnDelegate += MidiKeyPress;
+    }
+
+
+    void MidiKeyPress(MidiChannel channel, int note, float velocity)
+    {
+        timer = Time.time + timeUntilFadeIn;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		if (Time.time > timer)
+        {
+            FadeIn();
+        } else
+        {
+            FadeOut();
+        }
+	}
+
+    void FadeIn ()
+    {
+        colour.a += fadeSpeed;
+        spriteRenderer.color = colour;
+    }
+
+    void FadeOut()
+    {
+        colour.a -= fadeSpeed;
+        spriteRenderer.color = colour;
+    }
+}
